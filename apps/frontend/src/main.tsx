a16f125dee9e66b02dd3a5d28ed35bba2ac1e274
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-11-26 14:56:21 -0500

    chore(start): replace turbo repo starter code

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import * as TanStackQueryProvider from "./integrations/tanstack-query/root-provider.tsx";
 
 // Import the generated route tree
-import { routeTree } from "./routeTree.gen";
+import { routeTree } from "./routeTree.gen.ts";
 
 import "./styles.css";
 import reportWebVitals from "./reportWebVitals.ts";
