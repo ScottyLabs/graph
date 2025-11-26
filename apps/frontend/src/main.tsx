f3b0ada8e85bf7bb943b6dbfd561c03bf42153ac
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-11-26 15:49:39 -0500

    chore(start): add tsconfig

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,12 +3,9 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 
 import * as TanStackQueryProvider from "./integrations/tanstack-query/root-provider.tsx";
-
+import reportWebVitals from "./reportWebVitals.ts";
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen.ts";
-
-import "./styles.css";
-import reportWebVitals from "./reportWebVitals.ts";
 
 // Create a new router instance
 
