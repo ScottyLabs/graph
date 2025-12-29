60dd7169c5d89a1b847d266451e5c54dce90f07d
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-12-29 03:40:33 +0000

    chore: fix secrets scripts

--- scripts/secrets-pull.sh
+++ scripts/secrets-pull.sh
@@ -4,8 +4,8 @@ source scripts/config.sh
 usage() {
   echo
   echo -e "\tUsage: $0 APPLICATION ENVIRONMENT\n"
-  echo -e "\t\tAPPLICATION: The application to pull from, one of ${APPLICATIONS_OPTIONS[@]} | all\n"
-  echo -e "\t\tENVIRONMENT: The environment to pull from, one of ${ENVIRONMENTS_OPTIONS[@]} | all\n"
+  echo -e "\t\tAPPLICATION: The application to pull from, one of $APPLICATIONS_OPTIONS_JOINED | all\n"
+  echo -e "\t\tENVIRONMENT: The environment to pull from, one of $ENVIRONMENTS_OPTIONS_JOINED | all\n"
   echo -e "\tOptions:"
   echo -e "\t\t-h, --help    Show this help message and exit\n"
 }
@@ -36,16 +36,20 @@ done
 if [ "$APPLICATION" == "all" ]; then
   APPLICATIONS=("${APPLICATIONS_OPTIONS[@]}")
 else
-  case "$APPLICATION" in
-  "${APPLICATIONS_OPTIONS[@]}")
-    APPLICATIONS=("$APPLICATION")
-    ;;
-  *)
+  valid=false
+  for opt in "${APPLICATIONS_OPTIONS[@]}"; do
+    if [ "$APPLICATION" == "$opt" ]; then
+      APPLICATIONS=("$APPLICATION")
+      valid=true
+      break
+    fi
+  done
+
+  if [ "$valid" == false ]; then
     echo "Error: Invalid application: '$APPLICATION'" >&2
     usage
     exit 1
-    ;;
-  esac
+  fi
 fi
 
 # Sanitize the Environment argument
